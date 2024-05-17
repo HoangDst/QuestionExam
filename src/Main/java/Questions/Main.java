9b37ce6e9cc8c73614cea1d0f6557b5dce7a9927
Home <minh.nt0275@gmail.com>
2024-05-18 01:08:52 +0700

    Pig

--- src/Main/java/Questions/Main.java
+++ src/Main/java/Questions/Main.java
@@ -2,34 +2,22 @@ package Questions;
 
 import Exams.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-// Press Shift twice to open the Search Everywhere dialog and type show whitespaces,
-// then press Enter. You can now see whitespace characters in your code.
+import Lib.Table;
 public class Main {
     public static void main(String[] args) {
-        QuestionRepository qr = new QuestionRepository();
-        //qr.addQuestion();
-        //qr.deleteDatabaseQuestions();
-        List<Question> questions = qr.getQuestions();
-        //for (Question q : questions) System.out.println(q + "\n\n\n");
-        //qr.updateQuestion(3);
-        //qr.deleteQuestion(1);
-        //List<Question> fq = qr.filter();
-        //System.out.println("\n\n\n");
-        //for (Question q : fq) System.out.println(q);
 
         ExamRepository er = new ExamRepository();
-        System.out.println("\n\n\n");
         List<Exam> exams = er.getExams();
-        for (Exam e : exams) System.out.println(e + "\n\n\n");
-        er.removeExam(2);
-        for (Exam e : exams) System.out.println(e + "\n\n\n");
-        er.saveExam();
+        for (Exam e : exams) e.display();
 
-
-        //for (Question q : questions) System.out.println(q);
-        //qr.saveQuestions();
+/*
+        Table table = new Table("Firstname", "Lastname", "Email", "Phone");
+        table.setColumnAlignment(3, false);
+        table.addRow("John", "Doe", "johndoe@nothing.com", "+2137999999");
+        table.addRow("Jane", "Doe", "janedoe@nothin.com", "+21379aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9999");
+        System.out.println(table);*/
     }
 }
