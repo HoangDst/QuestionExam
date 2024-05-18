d44df6627227b437b42d25dbcb3a077568830ae1
HoangDst <quanhoangsocialwar15@gmail.com>
2024-05-19 00:07:44 +0700

    fix

--- src/Main/java/Questions/QuestionRepository.java
+++ src/Main/java/Questions/QuestionRepository.java
@@ -17,48 +17,108 @@ public class QuestionRepository {
         questions = new ArrayList<>();
         loadQuestions();
     }
+
     public List<Question> getQuestions() {
         return questions;
     }
+
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
     }
+
     public List<Question> filter() {
         HashMap<String, String> columnFilter = new HashMap<String, String>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter values to filter (Enter to skip):");
-        System.out.println("Question type (E/MC): ");
-        String typeF = scanner.nextLine();
+        String typeF;
+        while (true) {
+            System.out.println("Enter question type (E/MC): ");
+            typeF = scanner.nextLine();
+            if (typeF.isEmpty() || typeF.equalsIgnoreCase("E") || typeF.equalsIgnoreCase("MC")) break;
+            else System.out.println("Invalid type! Type must be 'E' or 'MC'");
+
+        }
         if (!typeF.isEmpty()) columnFilter.put("Type", typeF);
 
         // Grade
-        System.out.println("Grade: ");
-        String gradeF = scanner.nextLine();
-        if (!gradeF.isEmpty()) columnFilter.put("Grade", gradeF);
+        String gradeF;
+        while (true) {
+            try {
+                System.out.println("Enter grade: ");
+                gradeF = scanner.nextLine();
+                if (gradeF.isEmpty()) break;
+
+                int grade1 = Integer.parseInt(gradeF.trim());
+                if (grade1 >= 1 && grade1 <= 12) break;
+                else System.out.println("Invalid grade! Grade must be between 1 and 12");
+
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Must enter a integer value! Try again");
+                scanner.nextLine();
+            }
+        }
+        if (!gradeF.isEmpty()) {
+            columnFilter.put("Grade", gradeF);
+        }
+
 
         // Subject
-        System.out.println("Subject: ");
+        System.out.println("Enter Subject: ");
         String subjectF = scanner.nextLine();
         if (!subjectF.isEmpty()) columnFilter.put("Subject", subjectF);
 
         // Chapter
-        System.out.println("Chapter: ");
+        System.out.println("Enter Chapter: ");
         String chapterF = scanner.nextLine();
         if (!chapterF.isEmpty()) columnFilter.put("Chapter", chapterF);
 
         // Difficulty
-        System.out.println("Difficulty: ");
-        String difficultyF = scanner.nextLine();
+        String difficultyF;
+        while (true) {
+            try {
+                System.out.println("Enter difficulty (1 for Easy / 2 for Medium / 3 for Hard): ");
+                difficultyF = scanner.nextLine();
+                if (difficultyF.isEmpty()) break;
+
+                int difficulty1 = Integer.parseInt(difficultyF.trim());
+                if (difficulty1 >= 1 && difficulty1 <= 3) break;
+                else System.out.println("Invalid difficulty! Difficulty must be 1, 2 or 3");
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Must enter a integer value! Try again");
+                scanner.nextLine();
+            }
+
+        }
         if (!difficultyF.isEmpty()) columnFilter.put("Difficulty", difficultyF);
 
         // Score
-        System.out.println("Score: ");
-        String scoreF = scanner.nextLine();
+        String scoreF;
+        while (true) {
+
+            try {
+                System.out.println("Enter score (Scale of 10): ");
+                scoreF = scanner.nextLine();
+                if (scoreF.isEmpty()) break;
+                double score1 = Double.parseDouble(scoreF.trim());
+                if (score1 >= 1 && score1 <= 10) {
+                    break;
+                } else {
+                    System.out.println("Invalid score! Score must be 10-point scale");
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Must enter a double value! Try again");
+                scanner.nextLine();
+            }
+        }
         if (!scoreF.isEmpty()) columnFilter.put("Score", scoreF);
 
 
         if (columnFilter.size() > 0) {
             List<Question> filteredQuestions = new ArrayList<>();
+            boolean foundMatchingQuestions = false;
             int size = columnFilter.size();
             String query = "SELECT * FROM questions WHERE ";
             for (String c : columnFilter.keySet()) {
@@ -69,11 +129,13 @@ public class QuestionRepository {
 
             try (PreparedStatement preparedStatement = connector.getConnection().prepareStatement(query)) {
                 int index = 1;
+
                 for (String c : columnFilter.keySet()) {
                     preparedStatement.setString(index++, columnFilter.get(c));
                 }
                 ResultSet resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
+                    foundMatchingQuestions = true;
                     int id = resultSet.getInt("ID");
                     String type = resultSet.getString("Type");
                     int grade = resultSet.getInt("Grade");
@@ -102,6 +164,12 @@ public class QuestionRepository {
                         filteredQuestions.add(eq);
                     }
                 }
+                if (!foundMatchingQuestions) {
+                    System.out.println("No question found");
+                }
+                for (Question q : filteredQuestions) {
+                    System.out.println(q);
+                }
                 return filteredQuestions;
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -109,33 +177,73 @@ public class QuestionRepository {
         }
         return questions;
     }
+
     public boolean questionExist(int id) {
         for (Question q : questions) {
             if (q.getId() == id) return true;
         }
         return false;
     }
+
     public Question getQuestion(int id) {
         for (Question q : questions) {
             if (q.getId() == id) return q;
         }
         return null;
     }
+
+    //chưa xử lý
     public void addQuestion() {
         Scanner scanner = new Scanner(System.in);
         List<Answer> answers = new ArrayList<>();
-        System.out.println("Enter question type (E/MC): ");
-        String type = scanner.nextLine();
-        System.out.println("Enter grade: ");
-        int grade = scanner.nextInt();
+
+        String type;
+        while (true) {
+            System.out.println("Enter question type (E/MC): ");
+            type = scanner.nextLine();
+            if (type.equalsIgnoreCase("E") || type.equalsIgnoreCase("MC")) {
+                break;
+            } else {
+                System.out.println("Invalid type! Type must be 'E' or 'MC'");
+            }
+        }
+
+        int grade;
+        while (true) {
+            try {
+                System.out.println("Enter grade: ");
+                grade = scanner.nextInt();
+                if (grade >= 1 && grade <= 12) break;
+                else System.out.println("Invalid grade! Grade must be between 1 and 12");
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Must enter a integer value! Try again");
+                scanner.nextLine();
+            }
+        }
+
         System.out.println("Enter subject: ");
-        scanner.nextLine(); // Clear residual newline character
+        scanner.nextLine();
         String subject = scanner.nextLine();
+
         System.out.println("Enter chapter: ");
         String chapter = scanner.nextLine();
-        System.out.println("Enter difficulty: ");
-        int difficulty = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
+
+        int difficulty;
+        while (true) {
+            try {
+                System.out.println("Enter difficulty (1 for Easy / 2 for Medium / 3 for Hard): ");
+                difficulty = scanner.nextInt();
+                if (difficulty >= 1 && difficulty <= 3) break;
+                else System.out.println("Invalid difficulty! Difficulty must be 1, 2 or 3");
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Must enter a integer value! Try again");
+                scanner.nextLine();
+            }
+        }
+
+        scanner.nextLine();
 
         System.out.println("Enter question: ");
         String question = scanner.nextLine();
@@ -143,14 +251,17 @@ public class QuestionRepository {
         if (type.equalsIgnoreCase("MC")) {
             int numChoice = 0;
             while (true) {
-                System.out.println("Enter number of choices: ");
-                numChoice = scanner.nextInt();
-                scanner.nextLine(); // Consume the newline character
+                try {
+                    System.out.println("Enter number of choices: ");
+                    numChoice = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
 
-                if (numChoice > 0 && numChoice <= 10) {
-                    break;
-                } else {
-                    System.out.println("Invalid number! Please try again");
+                    if (numChoice > 0 && numChoice <= 10) break;
+                    else System.out.println("Invalid number! Please try again");
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Must enter a integer value! Try again");
+                    scanner.nextLine();
                 }
             }
 
@@ -159,8 +270,21 @@ public class QuestionRepository {
             answers = mcq.getAnswers();
             System.out.println("Enter suggestion: ");
             String suggestion = scanner.nextLine();
-            System.out.println("Enter score: ");
-            double score = scanner.nextDouble();
+
+            double score;
+            while(true) {
+                try {
+                    System.out.println("Enter score (Scale of 10): ");
+                    score = scanner.nextDouble();
+                    if (score >= 0 && score <= 10) break;
+                    else System.out.println("Invalid score! Score must be 10-point scale");
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Must enter a double value! Try again");
+                    scanner.nextLine();
+                }
+            }
+
             scanner.nextLine(); // Consume the newline character
 
             mcq = new MultipleChoiceQuestion(nextId, grade, subject, chapter, difficulty,
@@ -170,8 +294,21 @@ public class QuestionRepository {
         } else {
             System.out.println("Enter suggestion: ");
             String suggestion = scanner.nextLine();
-            System.out.println("Enter score: ");
-            double score = scanner.nextDouble();
+
+            double score;
+            while(true) {
+                try {
+                    System.out.println("Enter score (Scale of 10): ");
+                    score = scanner.nextDouble();
+                    if (score >= 0 && score <= 10) break;
+                    else System.out.println("Invalid score! Score must be 10-point scale");
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Must enter a double value! Try again");
+                    scanner.nextLine();
+                }
+            }
+
             scanner.nextLine(); // Consume the newline character
 
             EssayQuestion eq = new EssayQuestion(nextId, grade, subject, chapter, difficulty,
@@ -182,44 +319,90 @@ public class QuestionRepository {
 
         scanner.close();
     }
+
     public void updateQuestion(int id) {
         for (Question q : questions) {
             if (q.getId() == id) {
                 Scanner scanner = new Scanner(System.in);
+
+                int difficulty;
+                while (true) {
+                    try {
+                        System.out.println("Enter difficulty (1 for Easy / 2 for Medium / 3 for Hard): ");
+                        difficulty = scanner.nextInt();
+                        scanner.nextLine();
+                        if (difficulty >= 1 && difficulty <= 3) break;
+                        else System.out.println("Invalid difficulty! Difficulty must be 1, 2 or 3");
+                    }
+                    catch (InputMismatchException e) {
+                        System.out.println("Must enter a integer value! Try again");
+                        scanner.nextLine();
+                    }
+                }
+
+
                 System.out.println("Enter new question: ");
                 String question = scanner.nextLine();
                 if (q instanceof MultipleChoiceQuestion) {
                     MultipleChoiceQuestion mcq = (MultipleChoiceQuestion) q;
                     int numChoice = 0;
                     while (true) {
-                        System.out.println("Enter number of choices: ");
-                        numChoice = scanner.nextInt();
-                        if (numChoice > 0 && numChoice <= 10) break;
-                        else System.out.println("Invalid number! Please try again");
+                        try {
+                            System.out.println("Enter number of choices: ");
+                            numChoice = scanner.nextInt();
+                            scanner.nextLine(); // Consume the newline character
+
+                            if (numChoice > 0 && numChoice <= 10) break;
+                            else System.out.println("Invalid number! Please try again");
+                        }
+                        catch (InputMismatchException e) {
+                            System.out.println("Must enter a integer value! Try again");
+                            scanner.nextLine();
+                        }
                     }
                     mcq.addAnswer(numChoice);
                 }
+
+                double score;
+                while(true) {
+                    try {
+                        System.out.println("Enter score (Scale of 10): ");
+                        score = scanner.nextDouble();
+                        if (score >= 0 && score <= 10) break;
+                        else System.out.println("Invalid score! Score must be 10-point scale");
+                    }
+                    catch (InputMismatchException e) {
+                        System.out.println("Must enter a double value! Try again");
+                        scanner.nextLine();
+                    }
+                }
+
                 q.setQuestion(question);
+                q.setDifficulty(difficulty);
+                q.setScore(score);
                 break;
             }
         }
     }
+
     public void deleteQuestion(int id) {
         for (Question q : questions) {
             if (q.getId() == id) questions.remove(q);
             break;
         }
     }
+
     private void deleteDatabaseQuestions() {
         String query = "DELETE FROM answers";
         connector.execution(query);
         query = "DELETE FROM questions";
         connector.execution(query);
     }
+
     public void saveQuestions() {
         deleteDatabaseQuestions();
         for (Question q : questions) {
-            String query = "INSERT INTO Questions (ID, Type, Grade, Subject, Chapter, Difficulty, Question, Suggestion, Score) " +
+            String query = "INSERT INTO questions (ID, Type, Grade, Subject, Chapter, Difficulty, Question, Suggestion, Score) " +
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
             List<Object> parameters = Arrays.asList(
                     q.getId(),
@@ -239,6 +422,7 @@ public class QuestionRepository {
             }
         }
     }
+
     public void loadQuestions() {
         if (!questions.isEmpty()) questions.clear();
         String query = "SELECT * FROM questions";
@@ -273,7 +457,7 @@ public class QuestionRepository {
                     questions.add(eq);
                 }
             }
-            if(!questions.isEmpty()) nextId = questions.get(questions.size() - 1).getId() + 1;
+            if (!questions.isEmpty()) nextId = questions.get(questions.size() - 1).getId() + 1;
         } catch (SQLException e) {
             e.printStackTrace();
         }
