aaeb7cae3130300ad2d889a59ec48e92c5149694
Linh <ngoclinh19092003@gmail.com>
2024-05-19 23:57:22 +0700

    Linh

--- src/Main/java/DatabaseConfiguration/Config.java
+++ src/Main/java/DatabaseConfiguration/Config.java
@@ -1,7 +1,7 @@
 package DatabaseConfiguration;
 
 public class Config {
-    public static final String DATABASE_URL = "jdbc:mysql://127.0.0.1:3306/exam";
-    public static final String DATABASE_USERNAME = "linh";
-    public static final String DATABASE_PASSWORD = "linh";
+    public static final String DATABASE_URL = "jdbc:mysql://127.0.0.1:3306/project";
+    public static final String DATABASE_USERNAME = "root";
+    public static final String DATABASE_PASSWORD = "123456";
 }
